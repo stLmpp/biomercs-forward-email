80194bbcd5f040616e22a3c5c2f79f0afe45c260
stLmpp <gui.stlmpp@hotmail.com>
2021-10-28 11:41:20 -0300

    - removed empty callback - added log messages

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { SESHandler } from 'aws-lambda';
 import { simpleParser } from 'mailparser';
 import { S3, SES } from 'aws-sdk';
 
+function log(message: string): void {
+  // eslint-disable-next-line no-console
+  console.log(message);
+}
+
 const handler: SESHandler = async event => {
   try {
+    log('E-mail received. Forwarding.');
+
     const ses = event.Records[0].ses;
     const bucketName = process.env.BIO_AWS_S3_BUCKET!;
     const key = ses.mail.messageId;
+
+    log(`Bucket: ${bucketName}, file: ${key}`);
 
     const s3Client = new S3({
       region: process.env.BIO_AWS_REGION!,
@@ -20,26 +29,27 @@ const handler: SESHandler = async event => {
 
     const html = typeof email.html === 'string' ? email.html : email.textAsHtml;
     await sesClient
-      .sendEmail(
-        {
-          Message: {
-            Body: {
-              Html: {
-                Data: `<p>E-mail from: ${email.from?.value
-                  .map(from => `${from.name} (${from.address ?? 'No address'})`)
-                  .join(', ')}</p><br><br>${html}`,
-              },
+      .sendEmail({
+        Message: {
+          Body: {
+            Html: {
+              Data: `<p>E-mail from: ${email.from?.value
+                .map(from => `${from.name} (${from.address ?? 'No address'})`)
+                .join(', ')}</p><br><br>${html}`,
             },
-            Subject: { Data: email.subject! },
           },
-          Destination: { ToAddresses: [process.env.BIO_MAIL_RECEIPT!] },
-          Source: process.env.BIO_MAIL_SENDER!,
+          Subject: { Data: email.subject! },
         },
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        () => {}
-      )
+        Destination: { ToAddresses: [process.env.BIO_MAIL_RECEIPT!] },
+        Source: process.env.BIO_MAIL_SENDER!,
+      })
       .promise();
+
+    log('E-mail sent!');
+
     await s3Client.deleteObject({ Bucket: bucketName, Key: key }).promise();
+
+    log('Object deleted from S3');
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
