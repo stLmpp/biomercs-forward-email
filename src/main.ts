55bfa81ebb611f78e7d0c80a3dfb0d8530cf77b7
stLmpp <gui.stlmpp@hotmail.com>
2021-10-28 12:17:09 -0300

    - removed try catch - check for supported types on object.Body

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { SESHandler } from 'aws-lambda';
 import { simpleParser } from 'mailparser';
 import { S3, SES } from 'aws-sdk';
+import { Stream } from 'stream';
 
 function log(message: string): void {
   // eslint-disable-next-line no-console
@@ -8,52 +9,52 @@ function log(message: string): void {
 }
 
 const handler: SESHandler = async event => {
-  try {
-    log('E-mail received. Forwarding.');
+  log('E-mail received. Forwarding.');
 
-    const ses = event.Records[0].ses;
-    const bucketName = process.env.BIO_AWS_S3_BUCKET!;
-    const key = ses.mail.messageId;
+  const ses = event.Records[0].ses;
+  const bucketName = process.env.BIO_AWS_S3_BUCKET!;
+  const key = ses.mail.messageId;
 
-    log(`Bucket: ${bucketName}, file: ${key}`);
+  log(`Bucket: ${bucketName}, file: ${key}`);
 
-    const s3Client = new S3({
-      region: process.env.BIO_AWS_REGION!,
-      apiVersion: 'latest',
-    });
+  const s3Client = new S3({
+    region: process.env.BIO_AWS_REGION!,
+    apiVersion: 'latest',
+  });
 
-    const object = await s3Client.getObject({ Bucket: bucketName, Key: key }).promise();
-    const email = await simpleParser(object.Body as any);
+  const object = await s3Client.getObject({ Bucket: bucketName, Key: key }).promise();
 
-    const sesClient = new SES({ region: process.env.BIO_AWS_REGION!, apiVersion: 'latest' });
-
-    const html = typeof email.html === 'string' ? email.html : email.textAsHtml;
-    await sesClient
-      .sendEmail({
-        Message: {
-          Body: {
-            Html: {
-              Data: `<p>E-mail from: ${email.from?.value
-                .map(from => `${from.name} (${from.address ?? 'No address'})`)
-                .join(', ')}</p><br><br>${html}`,
-            },
-          },
-          Subject: { Data: email.subject! },
-        },
-        Destination: { ToAddresses: [process.env.BIO_MAIL_RECEIPT!] },
-        Source: process.env.BIO_MAIL_SENDER!,
-      })
-      .promise();
-
-    log('E-mail sent!');
-
-    await s3Client.deleteObject({ Bucket: bucketName, Key: key }).promise();
-
-    log('Object deleted from S3');
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
+  if (typeof object.Body !== 'string' && !Buffer.isBuffer(object.Body) && !(object.Body instanceof Stream)) {
+    throw new Error('Object body not supported');
   }
+
+  const email = await simpleParser(object.Body);
+
+  const sesClient = new SES({ region: process.env.BIO_AWS_REGION!, apiVersion: 'latest' });
+
+  const html = typeof email.html === 'string' ? email.html : email.textAsHtml;
+  await sesClient
+    .sendEmail({
+      Message: {
+        Body: {
+          Html: {
+            Data: `<p>E-mail from: ${email.from?.value
+              .map(from => `${from.name} (${from.address ?? 'No address'})`)
+              .join(', ')}</p><br><br>${html}`,
+          },
+        },
+        Subject: { Data: email.subject! },
+      },
+      Destination: { ToAddresses: [process.env.BIO_MAIL_RECEIPT!] },
+      Source: process.env.BIO_MAIL_SENDER!,
+    })
+    .promise();
+
+  log('E-mail sent!');
+
+  await s3Client.deleteObject({ Bucket: bucketName, Key: key }).promise();
+
+  log('Object deleted from S3');
 };
 
 exports.handler = handler;
